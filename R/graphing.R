37bc0e6cf8aa9e6c1b87cb162ebdfc676056b1f4
steffilazerte <steffi@steffi.ca>
2016-04-27 09:44:13 -0700

    Update ggQQ

--- R/graphing.R
+++ R/graphing.R
@@ -25,44 +25,52 @@ ggQQ <- function(model, level = "all", plot = TRUE, title = NULL) {
     if(cls == "lme") rand <- nlme::ranef(model, level = level)[[1]]
     if(cls == "lmer") {
       rand <- lme4::ranef(model)
-      rand <- rand[[names(rand)[grepl(level, names(rand))]]][[1]]
+      rand <- rand[[names(rand)[grepl(paste0("^", level, "$"), names(rand))]]][[1]]
     }
     y <- rand[!is.na(rand)]
     m <- data.frame(n = 1, resid = rand)
     title <- paste0("QQ Plot: Random - ", level)
   }
 
-  # Setup for qqline
-  x <- qnorm(c(0.25,0.75))
-  y <- quantile(y, c(0.25, 0.75))
-  slope <- diff(y)/diff(x)
-  int <- y[1L] - slope * x[1L]
-
-  # Shapiro test statistic
-  if(nrow(m) > 2) {
-    s <- data.frame(p = round(shapiro.test(m$resid)$p.value, digits = 3),
-                    W = round(shapiro.test(m$resid)$statistic, digits = 3))
+  if(nrow(m) < 2) {
+    message("Fewer than 2 levels, no plot possible")
   } else {
-    message("Less than 3 data points: Can't compute Shapiro Test")
-    s <- data.frame(p = NA, W = NA)
-  }
+    # Setup for qqline
+    x <- qnorm(c(0.25,0.75))
+    y <- quantile(y, c(0.25, 0.75))
+    slope <- diff(y)/diff(x)
+    int <- y[1L] - slope * x[1L]
 
-  if(plot == TRUE){
-    p <- ggplot(data = m, aes(sample = resid)) +
-      theme_bw() +
-      stat_qq(alpha = 0.5) +
-      geom_abline(slope = slope, intercept = int, color="blue") +
-      labs(y = "Sample Residuals",
-           x = "Theoretical Quantiles",
-           title = ifelse(is.null(title), "QQ Plot", title)) +
-      annotate(geom = "text",
-               x = -Inf, y = +Inf,
-               hjust = 0, vjust = 1,
-               size = 3,
-               label = paste0("Shapiro Test \np = ", s$p, "\nW = ", s$W))
-    return(p)
-  } else {
-    return(s)
+    # Shapiro test statistic
+    if(nrow(m) > 2 | all(m$resid == 0)) {
+      s <- data.frame(p = round(shapiro.test(m$resid)$p.value, digits = 3),
+                      W = round(shapiro.test(m$resid)$statistic, digits = 3))
+    } else if(nrow(m) <= 2) {
+      message(paste0("Less than 3 data points in '", level, "': Can't compute Shapiro Test"))
+      s <- data.frame(p = NA, W = NA)
+    } else if(sd(m$resid) == 0) {
+      message(paste0("Too little variability in '", level, "': Can't compute Shapiro Test"))
+      s <- data.frame(p = NA, W = NA)
+    }
+
+
+    if(plot == TRUE){
+      p <- ggplot(data = m, aes(sample = resid)) +
+        theme_bw() +
+        stat_qq(alpha = 0.5) +
+        geom_abline(slope = slope, intercept = int, color="blue") +
+        labs(y = "Sample Residuals",
+             x = "Theoretical Quantiles",
+             title = ifelse(is.null(title), "QQ Plot", title)) +
+        annotate(geom = "text",
+                 x = -Inf, y = +Inf,
+                 hjust = 0, vjust = 1,
+                 size = 3,
+                 label = paste0("Shapiro Test \np = ", s$p, "\nW = ", s$W))
+      return(p)
+    } else {
+      return(s)
+    }
   }
 }
 
